c903cf1314815f31c268e796f6761b209af482ab
NovikovNS <50330986+NovikovNS@users.noreply.github.com>
2021-03-02 22:48:42 +0300

    Задание№2
    Создание класса Point.
    Создание функции distance
    Создание runnable class Distance с функцией distance
    Реализация метода в классе Point

---
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Distance.java
@@ -0,0 +1,12 @@
+package ru.stqa.pft.sandbox;
+
+public class Distance {
+  public static void main(String[] args) {
+
+    Point p1 = new Point(-1, 2);
+    Point p2 = new Point(2, -1);
+
+    System.out.println(p1.distance(p2));
+  }
+
+}
