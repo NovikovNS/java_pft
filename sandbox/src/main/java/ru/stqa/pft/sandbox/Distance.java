5fcb4f4fb220d38132df4a42e5d865ab7934825d
NovikovNS <50330986+NovikovNS@users.noreply.github.com>
2021-03-05 00:14:05 +0300

    Работа с тестами SquareTests

--- sandbox/src/main/java/ru/stqa/pft/sandbox/Distance.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Distance.java
@@ -3,8 +3,8 @@ package ru.stqa.pft.sandbox;
 public class Distance {
   public static void main(String[] args) {
 
-    Point p1 = new Point(-1, 2);
-    Point p2 = new Point(2, -1);
+    Point p1 = new Point(8, -1);
+    Point p2 = new Point(4, 2);
 
     System.out.println(p1.distance(p2));
   }
