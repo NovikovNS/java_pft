8ad9c6a802bfb29a97fea84849328c7f80f1ffba
NovikovNS <50330986+NovikovNS@users.noreply.github.com>
2021-03-21 12:29:14 +0300

    Работа с коллекциями
    Изменения вплоть до лекции 4.9 с лямбда-выражениями
    Добавление проверок в тесты по модификации, удалению и созданию групп

---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
@@ -0,0 +1,34 @@
+package ru.stqa.pft.addressbook.tests;
+
+import org.testng.Assert;
+import org.testng.annotations.*;
+import ru.stqa.pft.addressbook.model.GroupData;
+import java.util.HashSet;
+import java.util.List;
+
+public class GroupCreationTests extends TestBase {
+
+  @Test
+  public void testGroupCreation() {
+    app.getNavigationHelper().gotoGroupPage();
+    List<GroupData> before = app.getGroupHelper().getGroupList();
+    GroupData group = new GroupData("test2", null, null);
+    app.getGroupHelper().createGroup(group);
+    List<GroupData> after = app.getGroupHelper().getGroupList();
+    Assert.assertEquals(after.size(), before.size() + 1);
+
+    /*
+    int max = 0;
+    for (GroupData q: after) {
+      if (q.getId() > max) {
+        max = q.getId();
+      }
+    }
+    */
+
+    group.setId(after.stream().max((o1, o2) -> Integer.compare(o1.getId(), o2.getId())).get().getId());
+    before.add(group);
+    Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
+
+  }
+}
