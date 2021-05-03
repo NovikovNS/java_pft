37a0ae96b15c9fc2a7e89d73a24ca172662e78c4
NovikovNS <50330986+NovikovNS@users.noreply.github.com>
2021-05-03 20:37:25 +0300

    Лекция 9.6. REST: взаимодействие с GitHub
    Реализация модуля для взаимодействия с GitHub

---
+++ github-sample/src/test/java/ru/stqa/pft/github/GithubTests.java
@@ -0,0 +1,19 @@
+package ru.stqa.pft.github;
+
+import com.jcabi.github.*;
+import jersey.repackaged.com.google.common.collect.ImmutableMap;
+import org.testng.annotations.Test;
+
+import java.io.IOException;
+
+public class GithubTests {
+
+  @Test
+  public void testCommits() throws IOException {
+    Github github = new RtGithub("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");
+    RepoCommits commits = github.repos().get(new Coordinates.Simple("NovikovNS", "java_pft")).commits();
+    for (RepoCommit commit : commits.iterate(new ImmutableMap.Builder<String, String>().build())) {
+      System.out.println(new RepoCommit.Smart(commit).message());
+    }
+  }
+}
